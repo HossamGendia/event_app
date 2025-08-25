2376c7f0cc54d737726318c555adb4930cc4c479
HossamHassan <hhgendia2005@gmail.com>
2025-08-25 17:55:02 +0300

    setup firebase in work

--- lib/modules/splash/splash_screen.dart
+++ lib/modules/splash/splash_screen.dart
@@ -16,7 +16,7 @@ class _SplashScreenState extends State<SplashScreen> {
     // TODO: implement initState
     super.initState();
 
-    Future.delayed(const Duration(seconds: 3), () {
+    Future.delayed(const Duration(seconds: 1), () {
       Navigator.pushReplacementNamed(context, PageRoutesName.login);
     });
   }
